b79b98aa3e9fb1022ac4045e36bcb9c0c068b4ca
Leonid <leomalinovsky@gmail.com>
2018-06-06 18:22:53 +0300

    responsive fix 0.6 + form added

--- php/mail.php
+++ php/mail.php
@@ -1,20 +1,35 @@
 <?php
+
 if (!empty($_POST['name']) and !empty($_POST['email'])){
 
-	$theme = 'ТЕСТ РАБОТЫ ФОРМЫ';
+	$theme = 'БИЗНЕС. ЭМОЦИИ. ЛЮБОВЬ';
 	$name = $_POST['name'];
 	$email = $_POST['email'];
 
-	$to = .htmlspecialchars($email);
-	$letter = 'Здравствуйте, '.htmlspecialchars($name).'<br/><br/>
-	Спасибо Вам за отправленный запрос!'
+	$to = htmlspecialchars($email);
+	$letter = 'Здравствуйте, '.htmlspecialchars($name).'!<br/><br/>
+	Имя: '.htmlspecialchars($name).'<br/>
+	Email: '.htmlspecialchars($email).'<br/>
+	Спасибо Вам за отправленный запрос!<br/><br/>
+	С уважением,<br/>
+	Михаил Филяев<br/>';
 
-	$headers = "content-type: text/html; charset=utf-8\r\n";	
-	$headers .= "От Михаила Филяева";
+	$to2 = 'info@psy20.com';
+	$letter2 = 'ПОЛЕЗНЫЙ БОНУС запрошен<br/><br/>
+	Имя: '.htmlspecialchars($name).'<br/>
+	Email: '.htmlspecialchars($email).'<br/>';
+
+	$headers = "content-type: text/html; charset=utf-8\r\n";
+	$headers .="From: 'Михаил Филяев'";
+	
 	mail($to, $theme , $letter, $headers);
-	// header("location: ../contacts.html");
-	echo "SUCCESS!";
+	mail($to2, $theme , $letter2, $headers);
+	header("location: ../index.html");
+
+ 	echo "SUCCESS!";
+
 }else{
- echo "ERROR!";
+	 echo "ERROR!";
 }
+
 ?>
